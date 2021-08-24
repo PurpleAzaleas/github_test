f4220411568b62ac0de22c340a8ab31e05ee0dce
hu <958187794@qq.com>
2021-08-24 15:34:16 +0800

    springboot_demo 7.0版本

--- src/main/java/com/example/GitHubTest.java
+++ src/main/java/com/example/GitHubTest.java
@@ -3,7 +3,6 @@ package com.example;
 public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("test");
-        int a = 1;
         int c = 3;
         String s = "test";
     }
